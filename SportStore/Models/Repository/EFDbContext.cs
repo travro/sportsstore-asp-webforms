9eab556c0465097d51f233f1369f086f3c643e50
TravisWork <travis.melvin@sumtotalsystems.com>
2019-04-26 15:18:56 -0400

    ADD boiler plate code
    Added folders, models and dbcontext

---
+++ SportStore/Models/Repository/EFDbContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Data.Entity;
+
+namespace SportStore.Models.Repository
+{
+    public class EFDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
